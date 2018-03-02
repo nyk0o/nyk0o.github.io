5f03d35b5b7212be3704b7c19af63a83d2cfc05f
nyk0o <37002828+nyk0o@users.noreply.github.com>
2018-03-02 23:27:42 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+var myImage = document.querySelector('img');
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+	if(mySrc === 'images/bubble.jpeg') {
+		myImage.setAttribute ('src', 'images/peeking.PNG');
+	} else {
+		myImage.setAttribute ('src', 'images/bubble.jpeg');
+	}
+}
+/*For more features to manipulate documents, read: 
+* https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model
+*/
+
+
+//#####functions#####
+function multiply(num1,num2) {
+	//var result = num1 * num2;
+	return num1 * num2;
+}
+
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+	var myName = prompt('Please enter your name.');
+	localStorage.setItem('name', myName);
+	myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+}
+//~initialization:
+if(!localStorage.getItem('name')) {
+	setUserName();
+} else {
+	var storedName = localStorage.getItem('name');
+	myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick = function() {
+	setUserName();
+}
